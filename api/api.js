5b8dc89d27eb7f27423c9342d4c4b8df2ec803ef
Brenton Fleming <bkfl@deakin.edu.au>
2018-07-24 09:54:26 +1000

    NEW: Add api functionility for user registration and authentication

--- api/api.js
+++ api/api.js
@@ -6,14 +6,17 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URL, { user: process.env.MONGO_USER, pass: process.env.MONGO_PASS, useNewUrlParser: true });
 
 const Device = require('./models/device');
+const User = require('./models/user');
 
 var bodyParser = require('body-parser');
 
+// Parse body data using body-parser package
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
 
+// Add header to allow multiple response origins
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content - Type, Accept");
@@ -50,6 +53,62 @@ app.post('/api/send-command', (req, res) => {
     console.log(req.body.command);
 });
 
+app.post('/api/authenticate', (req, res) => {
+    const { user, password } = req.body;
+    User.findOne({ user }, (err, found) => {
+        if (err) {
+            return res.send(err)
+        } else if (!found) {
+            return res.json({
+                success: false,
+                message: 'User not found',
+                isAdmin: false
+            });
+        } else if (password != found.password) {
+            return res.json({
+                success: false,
+                message: 'Incorrect password',
+                isAdmin: false
+            })
+        } else {
+            return res.json({
+                success: true,
+                message: 'Authenticated successfully',
+                isAdmin: found.isAdmin
+            });
+        }
+    });
+});
+
+app.post('/api/register', (req, res) => {
+    const { user, password, isAdmin } = req.body;
+    User.findOne({ user }, (err, found) => {
+        if (err) {
+            return res.send(err);
+        } else if (found) {
+            return res.json({
+                success: false,
+                message: 'User already exists'
+            });
+        } else {
+            const newUser = new User({
+                user,
+                password,
+                isAdmin
+            });
+            newUser.save(err => {
+                return err
+                    ? res.send(err)
+                    : res.json({
+                        success: true,
+                        message: 'Created new user'
+                    })
+            })
+        }
+    });
+});
+
+// Start listening
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
 });
